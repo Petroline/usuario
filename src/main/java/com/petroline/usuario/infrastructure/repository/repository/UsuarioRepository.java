5231705e71a635634b1b1adcf4a552ba774516bd
Everton <evertonpetroline@outlook.com>
2025-07-21 06:09:19 -0300

    Diminuindo o espacamento no UsuarioReposito

--- src/main/java/com/petroline/usuario/infrastructure/repository/repository/UsuarioRepository.java
+++ src/main/java/com/petroline/usuario/infrastructure/repository/repository/UsuarioRepository.java
@@ -1,6 +1,5 @@
 package com.petroline.usuario.infrastructure.repository.repository;
 
-
 import com.petroline.usuario.infrastructure.entity.Usuario;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,7 +13,6 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long > {
     boolean existsByEmail(String email);
 
    Optional<Usuario> findByEmail(String email);
-
 
    @Transactional
    void deleteByEmail(String email);
